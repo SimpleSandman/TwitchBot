789cbb2ee3db70dec3e96c538ecbc00fbb37a92c
SimpleSandman <simple.sandman@gmail.com>
2017-01-05 20:46:11 -0600

    Added table create scripts for stream ranking

---
+++ TwitchBot/TwitchBot/SQLTables/dbo.tblRankFollowers.sql
@@ -0,0 +1,18 @@
+﻿USE [twitchbotdb]
+GO
+
+/****** Object: Table [dbo].[tblRankFollowers] Script Date: 1/5/2017 8:43:55 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblRankFollowers] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [username]    VARCHAR (30) NOT NULL,
+    [exp]         INT          NOT NULL,
+    [broadcaster] INT          NOT NULL
+);
+
+
