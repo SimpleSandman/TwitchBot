a1bd95b53e2424e3aab1c73a122730a88b9a30b6
SimpleSandman <simple.sandman@gmail.com>
2017-02-27 18:47:47 -0600

    Moved SQL scripts and started on !bonusall cmd

---
+++ TwitchBot/TwitchBot/SqlScripts/UserTableTypes/dbo.tblBonusAll.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[tblBonusAll] AS TABLE (
+	[Id]          INT          NULL,
+	[username]    VARCHAR (30) NOT NULL,
+	[wallet]      INT          NOT NULL,
+	[broadcaster] INT		   NOT NULL,
+	[timeAdded]   DATETIME     DEFAULT (getdate()) NOT NULL
+);
