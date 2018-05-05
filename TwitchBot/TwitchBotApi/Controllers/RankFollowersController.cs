fe5d4e29994c3ffbe45e290ce1e0c9068943d4ca
SimpleSandman <simple.sandman@gmail.com>
2018-05-05 18:48:03 -0500

    Added CRUD for Ranks and started RankFollowers

---
+++ TwitchBot/TwitchBotApi/Controllers/RankFollowersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitchBotApi.DTO;
+using TwitchBotApi.Models;
+
+namespace TwitchBotApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class RankFollowersController : Controller
+    {
+        private readonly TwitchBotContext _context;
+
+        public RankFollowersController(TwitchBotContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/rankfollowers/get/2?username=simple_sandman
+        [HttpGet("{broadcasterId:int}")]
+        public async Task<IActionResult> Get([FromRoute] int broadcasterId, [FromQuery] string username)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            RankFollowers rankFollower = await _context.RankFollowers.SingleOrDefaultAsync(m => m.Broadcaster == broadcasterId && m.Username == username);
+
+            if (rankFollower == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rankFollower);
+        }
+
+        // GET: api/rankfollowers/getleaderboard/2?broadcastername=simple_sandman&botname=sandpaibot&topnumber=5
+        [HttpGet("{broadcasterId:int}")]
+        public async Task<IActionResult> GetLeaderboard([FromRoute] int broadcasterId, [FromQuery] BroadcasterConfig broadcasterConfig, [FromQuery] int topNumber = 3)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            List<RankFollowers> topFollowers = await _context.RankFollowers
+                .Where(m => m.Broadcaster == broadcasterId
+                    && m.Username != broadcasterConfig.BroadcasterName
+                    && m.Username != broadcasterConfig.BotName)
+                .OrderByDescending(m => m.Exp)
+                .Take(topNumber)
+                .ToListAsync();
+
+            if (topFollowers == null || topFollowers.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(topFollowers);
+        }
+
+        // PUT: api/rankfollowers/update/5
+        // Body (JSON): 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] RankFollowers rankFollowers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rankFollowers.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rankFollowers).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RankFollowersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/rankfollowers/create
+        // Body (JSON): { "username": "simple_sandman", "broadcaster": 2 }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] RankFollowers rankFollowers)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.RankFollowers.Add(rankFollowers);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RankFollowersExists(int id)
+        {
+            return _context.RankFollowers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TwitchBot/TwitchBotApi/Controllers/RanksController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitchBotApi.Models;
+
+namespace TwitchBotApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class RanksController : Controller
+    {
+        private readonly TwitchBotContext _context;
+
+        public RanksController(TwitchBotContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ranks/get/2
+        [HttpGet("{broadcasterId:int}")]
+        public async Task<IActionResult> Get([FromRoute] int broadcasterId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            List<Rank> rank = await _context.Rank.Where(m => m.Broadcaster == broadcasterId).ToListAsync();
+
+            if (rank == null || rank.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(rank);
+        }
+
+        // PUT: api/ranks/update/5?broadcasterId=2
+        // Body (JSON): 
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromQuery] int broadcasterId, [FromBody] Rank rank)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rank.Id || broadcasterId != rank.Broadcaster)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rank).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RankExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ranks/create
+        // Body (JSON): { "name": "New Rank", "expCap": 24, "broadcaster": 2 }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Rank rank)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Rank.Add(rank);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/ranks/delete/5?broadcasterId=2
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id, [FromQuery] int broadcasterId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Rank rank = await _context.Rank.SingleOrDefaultAsync(m => m.Id == id && m.Broadcaster == broadcasterId);
+            if (rank == null)
+            {
+                return NotFound();
+            }
+
+            _context.Rank.Remove(rank);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RankExists(int id)
+        {
+            return _context.Rank.Any(e => e.Id == id);
+        }
+    }
+}
