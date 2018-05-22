ffa2688682b6c1587a1f566e7e21cd5afeae4b7f
SimpleSandman <simple.sandman@gmail.com>
2018-05-21 23:08:45 -0500

    Added CRU (no DELETE) for BankHeistSettings and BossFightBossStats

---
+++ TwitchBot/TwitchBotApi/Controllers/BankHeistSettingsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitchBotApi.Models;
+
+namespace TwitchBotApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class BankHeistSettingsController : Controller
+    {
+        private readonly TwitchBotContext _context;
+
+        public BankHeistSettingsController(TwitchBotContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/bankheistsettings/get/2?id=1
+        [HttpGet("{broadcasterId:int}")]
+        public async Task<IActionResult> Get([FromRoute] int broadcasterId, [FromQuery] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            BankHeistSettings bankHeistSettings = await _context.BankHeistSettings.SingleOrDefaultAsync(m => m.Id == id && m.Broadcaster == broadcasterId);
+
+            if (bankHeistSettings == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bankHeistSettings);
+        }
+
+        // PUT: api/bankheistsettings/update/2?id=1
+        [HttpPut("{broadcasterId:int}")]
+        public async Task<IActionResult> Update([FromRoute] int broadcasterId, [FromQuery] int id, [FromBody] BankHeistSettings bankHeistSettings)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bankHeistSettings.Id || broadcasterId != bankHeistSettings.Broadcaster)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bankHeistSettings).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BankHeistSettingsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/bankheistsettings/create
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] BankHeistSettings bankHeistSettings)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BankHeistSettings.Add(bankHeistSettings);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BankHeistSettingsExists(int id)
+        {
+            return _context.BankHeistSettings.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TwitchBot/TwitchBotApi/Controllers/BossFightBossStatsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitchBotApi.Models;
+
+namespace TwitchBotApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class BossFightBossStatsController : Controller
+    {
+        private readonly TwitchBotContext _context;
+
+        public BossFightBossStatsController(TwitchBotContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/bossfightbossstats/get/1?settingsId=1
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get([FromRoute] int id, [FromQuery] int settingsId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            BossFightBossStats bossFightBossStats = await _context.BossFightBossStats.SingleOrDefaultAsync(m => m.Id == id && m.SettingsId == settingsId);
+
+            if (bossFightBossStats == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bossFightBossStats);
+        }
+
+        // PUT: api/bossfightbossstats/update/1?settingsId=1
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromQuery] int settingsId, [FromBody] BossFightBossStats bossFightBossStats)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bossFightBossStats.Id && settingsId != bossFightBossStats.SettingsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bossFightBossStats).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BossFightBossStatsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/bossfightbossstats/create
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] BossFightBossStats bossFightBossStats)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.BossFightBossStats.Add(bossFightBossStats);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BossFightBossStatsExists(int id)
+        {
+            return _context.BossFightBossStats.Any(e => e.Id == id);
+        }
+    }
+}
